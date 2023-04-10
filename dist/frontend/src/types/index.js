f39d90a5d64ee0ed393f305c4dc29b921cbef68f
Elliot Morrison <elliotmorrison58@gmail.com>
2023-04-10 16:49:05 -0500

    xd

--- dist/frontend/src/types/index.js
+++ dist/frontend/src/types/index.js
@@ -1,3 +1,21 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi8uLi8uLi9mcm9udGVuZC9zcmMvdHlwZXMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiJ9
+exports.Gender = exports.Size = exports.Role = void 0;
+var Role;
+(function (Role) {
+    Role["ADMIN"] = "ADMIN";
+    Role["USER"] = "USER";
+})(Role = exports.Role || (exports.Role = {}));
+var Size;
+(function (Size) {
+    Size["SMALL"] = "SMALL";
+    Size["MEDIUM"] = "MEDIUM";
+    Size["LARGE"] = "LARGE";
+})(Size = exports.Size || (exports.Size = {}));
+var Gender;
+(function (Gender) {
+    Gender["MALE"] = "MALE";
+    Gender["FEMALE"] = "FEMALE";
+    Gender["RATHER_NOT_SAY"] = "RATHER_NOT_SAY";
+})(Gender = exports.Gender || (exports.Gender = {}));
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi8uLi8uLi9mcm9udGVuZC9zcmMvdHlwZXMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7O0FBWUEsSUFBWSxJQUdUO0FBSEgsV0FBWSxJQUFJO0lBQ1osdUJBQWUsQ0FBQTtJQUNmLHFCQUFhLENBQUE7QUFDZixDQUFDLEVBSFMsSUFBSSxHQUFKLFlBQUksS0FBSixZQUFJLFFBR2I7QUFFRixJQUFZLElBSVY7QUFKRixXQUFZLElBQUk7SUFDYix1QkFBZSxDQUFBO0lBQ2YseUJBQWlCLENBQUE7SUFDakIsdUJBQWUsQ0FBQTtBQUNqQixDQUFDLEVBSlUsSUFBSSxHQUFKLFlBQUksS0FBSixZQUFJLFFBSWQ7QUFFSCxJQUFhLE1BSVY7QUFKSCxXQUFhLE1BQU07SUFDZix1QkFBYSxDQUFBO0lBQ2IsMkJBQWlCLENBQUE7SUFDakIsMkNBQWlDLENBQUE7QUFDbkMsQ0FBQyxFQUpVLE1BQU0sR0FBTixjQUFNLEtBQU4sY0FBTSxRQUloQiJ9
